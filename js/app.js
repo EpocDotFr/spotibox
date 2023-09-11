7fca2a1a489fb38f9c8a4d76e2f9f047515cc077
EpocDotFr <contact@epoc.fr>
2023-09-11 13:53:23 +0200

    Stuff

--- js/app.js
+++ js/app.js
@@ -2,7 +2,24 @@
     'use strict';
 
     class DeezBox {
+        pusherKey = '54c230647abc0f6ac73d';
+        pusherCluster = 'eu';
+
+        deezerAppId = '632324';
+        deezerSecretKey = '881dcaf57780e1248aa589af44925c04';
+
         constructor() {
+            /*this.pusher = new Pusher(this.pusherKey, {
+              cluster: this.pusherCluster
+            });
+
+            this.pusherChannel = this.pusher.subscribe('deezbox');
+
+            this.deezer = DZ.init({
+                appId: this.deezerAppId,
+                //channelUrl: 'http://developers.deezer.com/examples/channel.php'
+            });*/
+
             /*Alpine.store('radios', [
                 {
                     title: 'PulsRadio 00s',
