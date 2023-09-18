5e8de44fe1ddd5ffe1297efc058d7d8bc0e70953
EpocDotFr <contact@epoc.fr>
2023-09-18 13:32:41 +0200

    Stuff

--- spotibox/routes.py
+++ spotibox/routes.py
@@ -6,3 +6,21 @@ from app import app, auth
 @auth.login_required()
 def home() -> str:
     return render_template('home.html')
+
+
+@app.route('/host')
+@auth.login_required()
+def host() -> str:
+    return render_template('host.html')
+
+
+@app.route('/listen')
+@auth.login_required()
+def listen() -> str:
+    return render_template('listen.html')
+
+
+@app.route('/<any(host,listen):mode>/<room>')
+@auth.login_required()
+def room(mode: str, room: str) -> str:
+    return render_template('room.html')
