2ce90bfd1c5e29dcaf9f92c8a72d5d82d7df937c
EpocDotFr <contact@epoc.fr>
2023-09-22 15:20:52 +0200

    Signin in

--- spotibox/routes.py
+++ spotibox/routes.py
@@ -1,9 +1,10 @@
-from flask import render_template, redirect, url_for, request, flash
+from flask import render_template, redirect, url_for, request, flash, abort
+from spotibox.spotify import create_auth_manager, create_api_client
 from flask_login import current_user, login_user, logout_user
-from spotibox.spotify import create_auth_manager
 from spotibox.models import User
+from sqlalchemy import select
 from werkzeug import Response
-from app import app
+from app import app, db
 
 
 @app.route('/')
@@ -40,24 +41,64 @@ def authorize_callback() -> Response:
 
             flash(f'Failed to get access token from Spotify.', 'danger')
 
-        # TODO récup infos user
-        # TODO Créer ou mettre à jour user
+            return redirect(url_for('home'))
 
-        # login_user(user, remember=True)
+        try:
+            client = create_api_client(auth_manager)
+
+            user_info = client.me()
+        except Exception:
+            app.logger.exception('Failed to get Spotify account information')
+
+            if not app.config['DEBUG'] and app.config['SENTRY_DSN']:
+                import sentry_sdk
+
+                sentry_sdk.capture_exception()
+
+            flash(f'Failed to get Spotify account information.', 'danger')
+
+            return redirect(url_for('home'))
+
+        if user_info['product'] != 'premium':
+            flash('Sorry, you must have a Spotify Premium subscription to use Spotibox.')
+
+            return redirect(url_for('home'))
+
+        user = db.session.scalar(select(User).where(User.spotify_id == user_info['id']))
+
+        if not user:
+            user = User()
+            user.spotify_id = user_info['id']
+
+        user.display_name = user_info['display_name']
+        user.profile_image_url = user_info['images'][0]['url'] if user_info['images'] else None # TODO Use the smallest one (>= 32 px)
+        user.access_token = 'todo' # TODO
+        user.refresh_token = 'todo' # TODO
+
+        db.session.add(user)
+        db.session.commit()
+
+        login_user(user, remember=True)
 
         flash('Successfully logged in.', 'success')
     elif error:
         if error == 'access_denied':
-            flash('You did not authorize Spotibox to access your Spotify account.', 'danger')
+            flash('You did not authorize Spotibox to access your Spotify account.', 'warning')
         else:
-            flash(f'Got error code "{error}" from Spotify.', 'danger')
+            flash(f'Got error code "{error}" from Spotify.', 'warning')
+    else:
+        abort(400)
 
     return redirect(url_for('home'))
 
 
 @app.route('/sign-out')
 def sign_out() -> Response:
-    # TODO access_token et refresh_token à null
+    current_user.access_token = None
+    current_user.refresh_token = None
+
+    db.session.add(current_user)
+    db.session.commit()
 
     logout_user()
 
