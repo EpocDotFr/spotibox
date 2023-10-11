1c5ea56e0018abad41b33b8a27f18e948e40aef5
EpocDotFr <contact@epoc.fr>
2023-10-11 21:18:50 +0200

    Remove Flask's empty static_url_path

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from environs import Env
 env = Env()
 env.read_env()
 
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__)
 
 app.config.update(
     # Default config values that may be overwritten by environment values
